14cc620b44866941cd6a1fe9bf1f7461cd50f221
YASH-ROCKSTAR <yash.nigam_cs19@gla.ac.in>
2021-10-17 13:09:28 +0530

    final commit

--- scipt.js
+++ scipt.js
@@ -1,3 +1,18 @@
+// var counter=0;
+
+// function increment(){
+//     updateDisplay(++counter);
+// }
+// function decrement(){
+    
+//     updateDisplay(--counter);  
+//     if(counter==0){
+//         document.getElementById("click").disabled=true;
+//     }
+// }
+// function updateDisplay(value){
+//     document.getElementById("click").innerHTML=value;
+// }
 var counter=0;
 
 function increment(){
@@ -5,9 +20,12 @@ function increment(){
 }
 function decrement(){
     
-    updateDisplay(--counter);  
     if(counter==0){
-        document.getElementById("click").ariaDisabled=true;
+        document.getElementById("click").disabled=true;
+    }
+    else{
+        updateDisplay(--counter);
+        
     }
 }
 function updateDisplay(value){
