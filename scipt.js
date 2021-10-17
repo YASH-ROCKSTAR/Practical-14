b4651f28a248acb8d12617bd28b3a2dc5ba4ebbd
YASH-ROCKSTAR <yash.nigam_cs19@gla.ac.in>
2021-10-17 13:11:11 +0530

    chnages

--- scipt.js
+++ scipt.js
@@ -1,18 +1,3 @@
-// var counter=0;
-
-// function increment(){
-//     updateDisplay(++counter);
-// }
-// function decrement(){
-    
-//     updateDisplay(--counter);  
-//     if(counter==0){
-//         document.getElementById("click").disabled=true;
-//     }
-// }
-// function updateDisplay(value){
-//     document.getElementById("click").innerHTML=value;
-// }
 var counter=0;
 
 function increment(){
